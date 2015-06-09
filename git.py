57b0b9def14b2ba75f74a86c6770a44c25c75608
KodaKDominus <kodakatsuro@gmail.com>
2015-06-09 22:08:51 +0200

    Back to simple context manager.

--- git.py
+++ git.py
@@ -1,44 +1,46 @@
 
 import os
+import re
 import subprocess
-from contextlib import ContextDecorator
 
 
-class cd(ContextDecorator):
-    """Context manager/decorator for changing the current working directory."""
+DATE_REGEX = '\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}'  # ISO 8601
+
+
+class cd:
+    """Context manager for changing the current working directory."""
     def __init__(self, new_path):
         self.new_path = os.path.expanduser(new_path)
 
     def __enter__(self):
         self.previous_path = os.getcwd()
         os.chdir(self.new_path)
-        return self
 
-    def __exit__(self, *exc):
+    def __exit__(self, etype, value, traceback):
         os.chdir(self.previous_path)
-        return False
 
 
-# TODO use same context for all methods
 class git:
-    """Minimal git wrapper, providing only funtions to init add and commit."""
-    path = ''
+    """Minimal git wrapper, providing only: init, add and commit."""
+
     def __init__(self, path):
+        # TODO create path if it does not exist?
         assert os.path.isabs(path)
         self.path = path
 
-    @cd(path)
     def init(self):
-        print('current dir: {}'.format(os.getcwd()))
-        subprocess.call('git init')
-
+        with cd(self.path):
+            subprocess.call('git init')
 
     def add(self, filenames):
-        for filename in filenames:
-            assert os.path.isfile(filename)  # needs entire path
-            subprocess.call('git add ' + filename)
+        with cd(self.path):
+            for filename in filenames:
+                assert os.path.isfile(filename)  # needs entire path
+                subprocess.call('git add ' + filename)
 
     def commit(self, date, message=''):
-        subprocess.call(
-            "git commit -m '{m}' --date {d}".format(m=message, d=date)
-        )
+        assert re.compile(DATE_REGEX).match(date)
+        with cd(self.path):
+            subprocess.call(
+                "git commit -m '{m}' --date {d}".format(m=message, d=date)
+            )
