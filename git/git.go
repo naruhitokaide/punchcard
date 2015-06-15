74c397d81842d70996f4dbcff19f9979f86c02d0
KodaKDominus <kodakatsuro@gmail.com>
2015-06-15 22:12:02 +0200

    Add docs for git.

--- git/git.go
+++ git/git.go
@@ -6,6 +6,8 @@ import (
 	"os/exec"
 )
 
+// Init initializes a git repo in the given path.
+// If the path does not already exists, it will be created.
 func Init(path string) {
 	if err := os.MkdirAll(path, os.ModeDir); err != nil {
 		log.Fatal(err)
@@ -16,6 +18,8 @@ func Init(path string) {
 	}
 }
 
+// Add wraps the git add call and will change into the path of the git repo
+// and add the file given by name.
 func Add(path, filename string) {
 	os.Chdir(path)
 	err := exec.Command("git", "add", filename).Run()
@@ -24,6 +28,8 @@ func Add(path, filename string) {
 	}
 }
 
+// Commit will change into the path of the git repo and execute git commit.
+// In addition a message and date for the commit are specified.
 func Commit(path, message, date string) {
 	os.Chdir(path)
 	err := exec.Command("git", "commit", "-m", message, "--date", date).Run()
