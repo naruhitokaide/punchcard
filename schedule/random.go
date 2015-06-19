8475165da04c8697f8227bdf42994b762b2d03c2
KodaKDominus <kodakatsuro@gmail.com>
2015-06-19 23:24:19 +0200

    Add docs and further layout implementation.

--- schedule/random.go
+++ schedule/random.go
@@ -1,23 +1,31 @@
 package schedule
 
 import (
-	"fmt"
 	"math/rand"
+	"time"
 )
 
+// RandomSchedule creates random commits over the past 365 days.
+// These commits will be created in the location specified in the command.
 func RandomSchedule(min, max) {
-	fmt.Println("Start random scheduler")
 
-	// representation of the past 365 days
-	// for each day
-	// get random value between --min and --max
-	rnd := getRandomNumber(min, max)
-	// save into structure representing the commits over the last year
-	// start worker, which will execute all commits using some sort of
-	// commit generator
+	days := getDaysSinceThisDayLastYear()
+	for day := range days {
+		rnd := getRandomNumber(min, max)
+		// save into structure representing the commits over the last year
+		// start worker, which will execute all commits using some sort of
+		// commit generator
+	}
 }
 
 // getRandomNumber returns a number in the range of min and max.
 func getRandomNumber(min, max int) int {
 	return rand.Intn(max-min) + min
+}
+
+// getDaysSinceThisDayLastYear returns a slice of days since todays date
+// last year. E.g. 01.01.2015 starts at the 01.01.2014.
+// Every day maps to itself minus one year except the 29.02 will map to 28.02.
+func getDaysSinceThisDayLastYear() []time.Date {
+	return
 }
