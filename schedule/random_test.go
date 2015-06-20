70f59fef9334c429c6950f210d23eaecac94feb0
KodaKDominus <kodakatsuro@gmail.com>
2015-06-20 23:55:08 +0200

    Add test cases for isLeapYear.

--- schedule/random_test.go
+++ schedule/random_test.go
@@ -1,3 +1,26 @@
 package schedule
 
+import (
+	"testing"
+	"time"
+)
+
+func TestIsLeapYear(t *testing.T) {
+	var tests = []struct {
+		date     time.Time
+		expected bool
+	}{
+		{time.Date(2009, time.November, 10, 0, 0, 0, 0, time.UTC), false},
+		{time.Date(2016, time.February, 28, 0, 0, 0, 0, time.UTC), false},
+		{time.Date(2016, time.February, 29, 0, 0, 0, 0, time.UTC), true},
+		{time.Date(2012, time.February, 29, 0, 0, 0, 0, time.UTC), true},
+	}
+	for _, test := range tests {
+		actual := isLeapDay(test.date)
+		if actual != test.expected {
+			fmt := "isLeapYear(%v) == %b; but wanted %b"
+			t.Errorf(fmt, test.date, actual, test.expected)
+		}
+	}
+}
 // TODO add test cases for time parsing
