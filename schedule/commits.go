38c08941f8e93abf3ce04593447aa05ac1cfde80
KodaKDominus <kodakatsuro@gmail.com>
2015-06-23 22:19:00 +0200

    Start implementing RandomCommits function.

--- schedule/commits.go
+++ schedule/commits.go
@@ -9,4 +9,23 @@ type Commit struct {
 	message  string
 }
 
-type Commits []Commit
+// RandomCommits returns a channel of random commits for a given day.
+func RandomCommits(day time.Time, rnd int) chan Commit {
+	commitChannel := make(chan Commit)
+	go func() {
+		for i := 0; i < rnd; i++ {
+			commitChannel <- Commit{
+				dateTime: getRandomTime(), message: getRandomCommitMessage(),
+			}
+		}
+	}()
+	return commitChannel
+}
+
+func getRandomTime() time.Time {
+	return time.Now()
+}
+
+func getRandomCommitMessage() string {
+	return "not so random string"
+}
