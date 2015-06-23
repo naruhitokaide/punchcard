269d99c6909c1305b038b70e2f2335d74c013d3d
KodaKDominus <kodakatsuro@gmail.com>
2015-06-23 22:18:18 +0200

    Split up random and general schedule functions.

--- schedule/random.go
+++ schedule/random.go
@@ -9,14 +9,15 @@ import (
 // RandomSchedule creates random commits over the past 365/366 days.
 // These commits will be created in the location specified in the command.
 func RandomSchedule(min, max int) {
-
+	// TODO add git init
 	days := getDaysSinceDateMinusOneYear(time.Now())
 	for day := range days {
 		rnd := getRandomNumber(min, max)
-		fmt.Println("%v - %d", day, rnd)
-		// save into structure representing the commits over the last year
-		// start worker, which will execute all commits using some sort of
-		// commit generator
+		commits := RandomCommits(day, rnd)
+		for commit := range commits {
+			// TODO git add and commit file with given
+			fmt.Println(commit.message)
+		}
 	}
 }
 
@@ -26,38 +27,4 @@ func getRandomNumber(min, max int) int {
 		return min
 	}
 	return rand.Intn(max-min) + min
-}
-
-// getDaysSinceDateMinusOneYear returns a slice of days since the given date
-// minus one year. E.g. 01.01.2015 starts at the 01.01.2014.
-func getDaysSinceDateMinusOneYear(givenDate time.Time) chan time.Time {
-	dayChannel := make(chan time.Time)
-	go func() {
-		day := getDayMinusOneYear(givenDate)
-		for givenDate.After(day) {
-			dayChannel <- day
-			day = day.AddDate(0, 0, 1)
-		}
-		// also add the givenDate, which will not be added using After()
-		dayChannel <- givenDate
-		close(dayChannel)
-	}()
-	return dayChannel
-}
-
-// getDayMinusOneYear returns the daya date minus one year, except the
-// 29.02 will map to 28.02.
-func getDayMinusOneYear(day time.Time) time.Time {
-	if isLeapDay(day) {
-		// adjust for one year and one day
-		return day.AddDate(-1, 0, -1)
-	} else {
-		return day.AddDate(-1, 0, 0)
-	}
-}
-
-// isLeapDay checks if a given datetime is the 29.02 or not.
-func isLeapDay(today time.Time) bool {
-	_, month, day := today.Date()
-	return (day == 29 && month == time.February)
 }
--- schedule/random_test.go
+++ schedule/random_test.go
@@ -2,50 +2,7 @@ package schedule
 
 import (
 	"testing"
-	"time"
 )
-
-func TestIsLeapYear(t *testing.T) {
-	var tests = []struct {
-		date     time.Time
-		expected bool
-	}{
-		{time.Date(2009, time.November, 10, 0, 0, 0, 0, time.UTC), false},
-		{time.Date(2016, time.February, 28, 0, 0, 0, 0, time.UTC), false},
-		{time.Date(2016, time.February, 29, 0, 0, 0, 0, time.UTC), true},
-		{time.Date(2012, time.February, 29, 0, 0, 0, 0, time.UTC), true},
-	}
-	for _, test := range tests {
-		actual := isLeapDay(test.date)
-		if actual != test.expected {
-			fmt := "isLeapYear(%v) == %b; but wanted %b"
-			t.Errorf(fmt, test.date, actual, test.expected)
-		}
-	}
-}
-
-func TestGetDateLastYear(t *testing.T) {
-	var tests = []struct {
-		date     time.Time
-		expected time.Time
-	}{
-		{time.Date(2009, time.November, 10, 0, 0, 0, 0, time.UTC),
-			time.Date(2008, time.November, 10, 0, 0, 0, 0, time.UTC)},
-		{time.Date(2016, time.February, 28, 0, 0, 0, 0, time.UTC),
-			time.Date(2015, time.February, 28, 0, 0, 0, 0, time.UTC)},
-		{time.Date(2016, time.February, 29, 0, 0, 0, 0, time.UTC),
-			time.Date(2015, time.February, 28, 0, 0, 0, 0, time.UTC)},
-		{time.Date(2012, time.February, 29, 0, 0, 0, 0, time.UTC),
-			time.Date(2011, time.February, 28, 0, 0, 0, 0, time.UTC)},
-	}
-	for _, test := range tests {
-		actual := getDayMinusOneYear(test.date)
-		if actual != test.expected {
-			fmt := "getDayLastYear(%v) == %v; but wanted %v"
-			t.Errorf(fmt, test.date, actual, test.expected)
-		}
-	}
-}
 
 func TestGetRandomNumber(t *testing.T) {
 	var tests = []struct {
@@ -63,31 +20,6 @@ func TestGetRandomNumber(t *testing.T) {
 		if test.min > actual || actual > test.max {
 			fmt := "getRandomNumber(%d, %d) == %d; not min <= actual <= max"
 			t.Errorf(fmt, test.min, test.max, actual)
-		}
-	}
-}
-
-func TestGetDaysSinceDateMinusOneYear(t *testing.T) {
-	var tests = []struct {
-		date           time.Time
-		expectedLength int
-	}{
-		{time.Date(2009, time.November, 10, 0, 0, 0, 0, time.UTC), 366},
-		{time.Date(2016, time.February, 28, 0, 0, 0, 0, time.UTC), 366},
-		{time.Date(2016, time.February, 29, 0, 0, 0, 0, time.UTC), 367},
-		{time.Date(2013, time.February, 28, 0, 0, 0, 0, time.UTC), 367},
-	}
-	for _, test := range tests {
-		actual := getDaysSinceDateMinusOneYear(test.date)
-		length := 0
-		for day := range actual {
-			// just logging so day is used here, otherwise it wouldn't compile
-			t.Log(day.String())
-			length++
-		}
-		if length != test.expectedLength {
-			fmt := "len(getDaysSinceDateMinusOneYear(%v)) == %d; but wanted %d"
-			t.Errorf(fmt, test.date, length, test.expectedLength)
 		}
 	}
 }
