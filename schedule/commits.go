996a7b4c3e3a7677e8f9d6b0e35b0c982ec99e41
KodaKDominus <kodakatsuro@gmail.com>
2015-06-24 22:35:28 +0200

    Move random logic to random.go

--- schedule/commits.go
+++ schedule/commits.go
@@ -4,29 +4,11 @@ import (
 	"time"
 )
 
+const (
+	COMMIT_MESSAGE_BASE = "commit_message_base.txt"
+)
+
 type Commit struct {
 	dateTime time.Time
 	message  string
-}
-
-// RandomCommits returns a channel of random commits for a given day.
-func RandomCommits(day time.Time, rnd int) chan Commit {
-	commitChannel := make(chan Commit)
-	go func() {
-		for i := 0; i < rnd; i++ {
-			commitChannel <- Commit{
-				dateTime: getRandomTime(), message: getRandomCommitMessage(),
-			}
-		}
-		close(commitChannel)
-	}()
-	return commitChannel
-}
-
-func getRandomTime() time.Time {
-	return time.Now()
-}
-
-func getRandomCommitMessage() string {
-	return "not so random string"
 }
