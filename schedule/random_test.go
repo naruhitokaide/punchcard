85d43f217ccc92288c080a56aababe42c1769660
KodaKDominus <kodakatsuro@gmail.com>
2015-06-25 22:48:38 +0200

    Add test cases for commit message and time.

--- schedule/random_test.go
+++ schedule/random_test.go
@@ -1,7 +1,9 @@
 package schedule
 
 import (
+	"strings"
 	"testing"
+	"time"
 )
 
 func TestGetRandomNumber(t *testing.T) {
@@ -24,4 +26,38 @@ func TestGetRandomNumber(t *testing.T) {
 	}
 }
 
-// TODO test random commit message and random time
+func TestGetRandomCommitMessage(t *testing.T) {
+	var tests = []struct {
+		length int
+	}{
+		{1}, {2}, {4}, {8},
+	}
+	for _, test := range tests {
+		actual := getRandomCommitMessage(test.length)
+		actualLength := len(strings.Split(actual, " "))
+		t.Log("Message: %s (length: %d)", actual, actualLength)
+		if actualLength < 0 || test.length < actualLength {
+			fmt := "getRandomCommitMessage(%d) == %s (length: %d)"
+			t.Errorf(fmt, test.length, actual, actualLength)
+		}
+	}
+}
+
+func TestGetRandomTime(t *testing.T) {
+	var tests = []struct {
+		date time.Time
+	}{
+		{time.Date(2009, time.November, 10, 0, 0, 0, 0, time.UTC)},
+		{time.Date(2016, time.February, 28, 0, 0, 0, 0, time.UTC)},
+	}
+	for _, test := range tests {
+		actual := getRandomTime(test.date)
+		t.Log("Date: %v and date with random time: %v", test.date, actual)
+		dayBefore := test.date.AddDate(0, 0, -1)
+		dayAfter := test.date.AddDate(0, 0, 1)
+		if actual.After(dayAfter) || actual.Before(dayBefore) {
+			fmt := "getRandomTime(%v) == %v; not dayBefore < actual < dayAfter"
+			t.Errorf(fmt, test.date, actual)
+		}
+	}
+}
