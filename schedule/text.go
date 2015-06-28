11380de84ff165537866badeb04f92140813bf30
KodaKDominus <kodakatsuro@gmail.com>
2015-06-28 21:07:32 +0200

    Add text as a command and add text schedule.

---
+++ schedule/text.go
@@ -0,0 +1,28 @@
+package schedule
+
+import (
+	"github.com/0xfoo/punchcard/git"
+	"github.com/0xfoo/punchcard/utils"
+	"time"
+)
+
+// TextSchedule creates commits over the past 365/366 days to build the given text.
+// These commits will be created in the given git repo using the FileGenerator.
+func TextSchedule(text string, repo git.Git, filegen utils.FileGenerator) error {
+	messageBase := GetCommitMessageBase()
+	days := GetDaysSinceNowMinusOneYear()
+	commits, err := GetTextCommitSchedule(text, days, messageBase)
+	if err != nil {
+		return err
+	}
+	for _, commit := range commits {
+		repo.Add(filegen.CreateFile())
+		repo.Commit(commit.Message, commit.DateTime.String())
+	}
+	return err
+}
+
+func GetTextCommitSchedule(text string, days []time.Time, messageBase []string) ([]Commit, error) {
+	// TODO
+	return nil, nil
+}
