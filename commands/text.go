48f9dcb466ff8f96615893a25f9a68ea6604a895
KodaKDominus <kodakatsuro@gmail.com>
2015-06-29 21:29:10 +0200

    Add text command (not added last time).

---
+++ commands/text.go
@@ -0,0 +1,43 @@
+package commands
+
+import (
+	"github.com/0xfoo/punchcard/git"
+	"github.com/0xfoo/punchcard/schedule"
+	"github.com/0xfoo/punchcard/utils"
+	"github.com/spf13/cobra"
+	"log"
+)
+
+const (
+	DEFAULT_TEXT = "HELLO WORLD"
+)
+
+var text string
+
+var textCmd = &cobra.Command{
+	Use:   "text",
+	Short: "Text will build the given text using commits.",
+	Long: `Text will create commits to display the given text on the github
+contribution graph. An error message will be shown in case the text will not
+fit in the graphs width.`,
+	Run: textRun,
+}
+
+// textRun creates repo and file generator based on the given location and
+// starts the TextSchedule to create the text in commits.
+func textRun(cmd *cobra.Command, args []string) {
+	repo := git.Repo{Location}
+	filegen := utils.RandomFileGenerator{Location}
+	repo.Init()
+	err := schedule.TextSchedule(text, repo, filegen)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// init initializes flags with default text and add textCmd to main cmd.
+func init() {
+	textCmd.Flags().StringVar(&text, "text", DEFAULT_TEXT,
+		"text which will build using commits")
+	PunchCardCmd.AddCommand(textCmd)
+}
