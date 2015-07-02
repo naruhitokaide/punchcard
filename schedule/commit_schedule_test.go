858ff5fb07a716a1bb400616ad29f05d79134448
KodaKDominus <kodakatsuro@gmail.com>
2015-07-02 22:52:16 +0200

    Add test cases for BuildCommitSchedule.

---
+++ schedule/commit_schedule_test.go
@@ -0,0 +1,44 @@
+package schedule
+
+import (
+	"testing"
+	"time"
+)
+
+func TestBuildCommitScheduleFullWeeks(t *testing.T) {
+	var tests = []struct {
+		startDay time.Time
+		numDays  int
+	}{
+		{time.Date(2009, time.November, 9, 0, 0, 0, 0, time.UTC), 7},
+		{time.Date(2015, time.November, 9, 0, 0, 0, 0, time.UTC), 21},
+		{time.Date(2014, time.July, 7, 0, 0, 0, 0, time.UTC), 365},
+	}
+	for _, test := range tests {
+		days := getTestDays(test.startDay, test.numDays)
+		schedule := BuildCommitSchedule(days)
+		// TODO add assertions
+	}
+}
+
+func TestBuildCommitScheduleEdgeCases(t *testing.T) {
+	var tests = []struct {
+		startDay time.Time
+		numDays  int
+	}{
+		{}, // TODO add edge cases
+	}
+	for _, test := range tests {
+		days := getTestDays(test.startDay, test.numDays)
+		schedule := BuildCommitSchedule(days)
+		// TODO add assertions
+	}
+}
+
+func getTestDays(startDay time.Time, numDays int) []time.Time {
+	var resultingDays []time.Time
+	for i := 0; i < numDays; i++ {
+		resultingDays = append(resultingDays, startDay.AddDate(0, 0, i))
+	}
+	return resultingDays
+}
