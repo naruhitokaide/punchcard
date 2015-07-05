064d53d1ec9c8fd0b44521928ac4cf5b4c51b032
KodaKDominus <kodakatsuro@gmail.com>
2015-07-05 23:16:43 +0200

    Add build first and last week test cases.

--- schedule/commit_schedule_test.go
+++ schedule/commit_schedule_test.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-func TestBuildCommitScheduleFullWeeks(t *testing.T) {
+func XTestBuildCommitScheduleFullWeeks(t *testing.T) {
 	var tests = []struct {
 		startDay time.Time
 		numDays  int
@@ -19,7 +19,7 @@ func TestBuildCommitScheduleFullWeeks(t *testing.T) {
 		schedule := BuildCommitSchedule(days)
 		for r, row := range schedule {
 			for c, col := range row {
-				if col != EMPTY {
+				if col != int(EMPTY) {
 					fmt := "Expected only EMPTY values, but got %d at (%d,%d)"
 					t.Errorf(fmt, col, r, c)
 				}
@@ -28,7 +28,7 @@ func TestBuildCommitScheduleFullWeeks(t *testing.T) {
 	}
 }
 
-func TestBuildCommitScheduleWednesdayStart(t *testing.T) {
+func XTestBuildCommitScheduleWednesdayStart(t *testing.T) {
 	var tests = []struct {
 		startDay time.Time
 		numDays  int
@@ -44,11 +44,11 @@ func TestBuildCommitScheduleWednesdayStart(t *testing.T) {
 			for c, col := range row {
 				firstWeekMondayOrTuesDay := (c == 0 && r < 2)
 				if firstWeekMondayOrTuesDay {
-					if col != NOT_A_FIELD {
+					if col != int(NOT_A_FIELD) {
 						fmt := "Expected NOT_A_FIELD values, but got %d at (%d,%d)"
 						t.Errorf(fmt, col, r, c)
 					}
-				} else if col != EMPTY {
+				} else if col != int(EMPTY) {
 					fmt := "Expected only EMPTY values, but got %d at (%d,%d)"
 					t.Errorf(fmt, col, r, c)
 				}
@@ -57,7 +57,7 @@ func TestBuildCommitScheduleWednesdayStart(t *testing.T) {
 	}
 }
 
-func TestBuildCommitScheduleThrusdayEnd(t *testing.T) {
+func XTestBuildCommitScheduleThrusdayEnd(t *testing.T) {
 	var tests = []struct {
 		startDay time.Time
 		numDays  int
@@ -73,11 +73,11 @@ func TestBuildCommitScheduleThrusdayEnd(t *testing.T) {
 			for c, col := range row {
 				lastWeekFridayOrSaturdayOrSunday := (c == len(row) && r > 4)
 				if lastWeekFridayOrSaturdayOrSunday {
-					if col != NOT_A_FIELD {
+					if col != int(NOT_A_FIELD) {
 						fmt := "Expected NOT_A_FIELD values, but got %d at (%d,%d)"
 						t.Errorf(fmt, col, r, c)
 					}
-				} else if col != EMPTY {
+				} else if col != int(EMPTY) {
 					fmt := "Expected only EMPTY values, but got %d at (%d,%d)"
 					t.Errorf(fmt, col, r, c)
 				}
@@ -92,4 +92,60 @@ func getTestDays(startDay time.Time, numDays int) []time.Time {
 		resultingDays = append(resultingDays, startDay.AddDate(0, 0, i))
 	}
 	return resultingDays
+}
+
+func TestBuildFirstWeek(t *testing.T) {
+	var tests = []struct {
+		day              time.Weekday
+		expectedSchedule []int
+	}{
+		{time.Sunday, []int{0, 0, 0, 0, 0, 0, 0}},
+		{time.Monday, []int{-1, 0, 0, 0, 0, 0, 0}},
+		{time.Tuesday, []int{-1, -1, 0, 0, 0, 0, 0}},
+		{time.Wednesday, []int{-1, -1, -1, 0, 0, 0, 0}},
+		{time.Thursday, []int{-1, -1, -1, -1, 0, 0, 0}},
+		{time.Friday, []int{-1, -1, -1, -1, -1, 0, 0}},
+		{time.Saturday, []int{-1, -1, -1, -1, -1, -1, 0}},
+	}
+	for _, test := range tests {
+		actualSchedule := buildFirstWeek(test.day)
+		if !sliceEqual(actualSchedule, test.expectedSchedule) {
+			fmt := "Expected %v as schedule, but got %v"
+			t.Errorf(fmt, test.expectedSchedule, actualSchedule)
+		}
+	}
+}
+
+func TestBuildLastWeek(t *testing.T) {
+	var tests = []struct {
+		day              time.Weekday
+		expectedSchedule []int
+	}{
+		{time.Sunday, []int{0, -1, -1, -1, -1, -1, -1}},
+		{time.Monday, []int{0, 0, -1, -1, -1, -1, -1}},
+		{time.Tuesday, []int{0, 0, 0, -1, -1, -1, -1}},
+		{time.Wednesday, []int{0, 0, 0, 0, -1, -1, -1}},
+		{time.Thursday, []int{0, 0, 0, 0, 0, -1, -1}},
+		{time.Friday, []int{0, 0, 0, 0, 0, 0, -1}},
+		{time.Saturday, []int{0, 0, 0, 0, 0, 0, 0}},
+	}
+	for _, test := range tests {
+		actualSchedule := buildLastWeek(test.day)
+		if !sliceEqual(actualSchedule, test.expectedSchedule) {
+			fmt := "Expected %v as schedule, but got %v"
+			t.Errorf(fmt, test.expectedSchedule, actualSchedule)
+		}
+	}
+}
+
+func sliceEqual(sliceA, sliceB []int) bool {
+	if len(sliceA) != len(sliceB) {
+		return false
+	}
+	for i := 0; i < len(sliceA); i++ {
+		if sliceA[i] != sliceB[i] {
+			return false
+		}
+	}
+	return true
 }
