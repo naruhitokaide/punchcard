ddb48659cc852a9d4394e45f591f131e8b5702c6
KodaKDominus <kodakatsuro@gmail.com>
2015-07-08 21:56:22 +0200

    Start implementing width checks.

--- schedule/text.go
+++ schedule/text.go
@@ -1,9 +1,14 @@
 package schedule
 
 import (
+	"errors"
 	"github.com/0xfoo/punchcard/git"
 	"github.com/0xfoo/punchcard/utils"
 	"time"
+)
+
+const (
+	SCHEDULE_WIDTH = 53
 )
 
 // TextSchedule creates commits over the past 365/366 days to build the given text.
@@ -24,8 +29,24 @@ func TextSchedule(text string, repo git.Git, filegen utils.FileGenerator) error 
 
 func getTextCommitSchedule(text string, days []time.Time, messageBase []string) ([]Commit, error) {
 	// TODO
-	// check if text can be put into commits
+	// check if text can be put into a commit schedule (width < 52)
+	if !textFits(text) {
+		return nil, errors.New("Text does not fit.")
+	}
 	// concatenate letters with one column as space between letters
-	// map the result onto commits using the given days
+	// put the result into a commit schedule
+	// translate the commit schedule to a []Commit
 	return nil, nil
+}
+
+func textFits(text string) bool {
+	textWidth := getTextWidth(text)
+	textIsNotToWide := textWidth <= SCHEDULE_WIDTH-2 // adjust for margins
+	textIsNotEmpty := textWidth > 0
+	return textIsNotEmpty && textIsNotToWide
+}
+
+func getTextWidth(text string) int {
+	// TODO translate into commit letters or some how get width
+	return len(text)
 }
