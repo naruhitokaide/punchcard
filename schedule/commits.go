7e5a6e1fefa5a949c131f563903eeb6ea8118837
KodaKDominus <kodakatsuro@gmail.com>
2015-07-18 22:40:15 +0200

    Use at least 2 words per commit message.

--- schedule/commits.go
+++ schedule/commits.go
@@ -14,7 +14,7 @@ type Commit struct {
 const (
 	COMMIT_MESSAGE_BASE       = `Non eram nescius, Brute, cum, quae summis ingeniis exquisitaque doctrina philosophi Graeco sermone tractavissent, ea Latinis litteris mandaremus, fore ut hic noster labor in varias reprehensiones incurreret. nam quibusdam, et iis quidem non admodum indoctis, totum hoc displicet philosophari. quidam autem non tam id reprehendunt, si remissius agatur, sed tantum studium tamque multam operam ponendam in eo non arbitrantur. erunt etiam, et ii quidem eruditi Graecis litteris, contemnentes Latinas, qui se dicant in Graecis legendis operam malle consumere. postremo aliquos futuros suspicor, qui me ad alias litteras vocent, genus hoc scribendi, etsi sit elegans, personae tamen et dignitatis esse negent.`
 	BASE_SEPARATOR            = " "
-	MIN_COMMIT_MESSAGE_LENGTH = 1
+	MIN_COMMIT_MESSAGE_LENGTH = 2
 	MAX_COMMIT_MESSAGE_LENGTH = 8
 )
 
