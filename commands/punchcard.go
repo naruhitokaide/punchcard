073ca89c5b5eb321fbd6b63fdb5dcc6b25475f5e
KodaKDominus <kodakatsuro@gmail.com>
2015-07-18 22:36:21 +0200

    Fix typo and use variable for default location.

--- commands/punchcard.go
+++ commands/punchcard.go
@@ -5,7 +5,8 @@ import (
 )
 
 const (
-	VERSION = "0.1.0"
+	VERSION          = "0.1.0"
+	DEFAULT_LOCATION = "."
 )
 
 var Location string
@@ -14,11 +15,11 @@ var PunchCardCmd = &cobra.Command{
 	Use:   "punchcard",
 	Short: "Punchcard is a fun tool to create fake git commits.",
 	Long: `Punchcard can create fake git commits in a repo.
-The larger purposes is to have fun with contribution graphs, punchcards etc.`,
+The larger purpose is to have fun with contribution graphs, punchcards etc.`,
 	Run: nil,
 }
 
 func init() {
-	PunchCardCmd.PersistentFlags().StringVar(&Location, "location", ".",
+	PunchCardCmd.PersistentFlags().StringVar(&Location, "location", DEFAULT_LOCATION,
 		"location where the git repo will be initialized")
 }
