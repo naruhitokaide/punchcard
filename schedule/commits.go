c0da7a0424978d3e113a8ed7c599913f2f20f21c
KodaKDominus <kodakatsuro@gmail.com>
2016-09-03 18:43:30 +0200

    Shorten commit message base
    Mostly so vim doesn't mess up the syntax highlighting, but it is a long
    line anyway; so shorting is not a bad idea.

--- schedule/commits.go
+++ schedule/commits.go
@@ -6,13 +6,14 @@ import (
 	"time"
 )
 
+// Commit represents a git commit given a time and a message.
 type Commit struct {
 	DateTime time.Time
 	Message  string
 }
 
 const (
-	COMMIT_MESSAGE_BASE       = `Non eram nescius, Brute, cum, quae summis ingeniis exquisitaque doctrina philosophi Graeco sermone tractavissent, ea Latinis litteris mandaremus, fore ut hic noster labor in varias reprehensiones incurreret. nam quibusdam, et iis quidem non admodum indoctis, totum hoc displicet philosophari. quidam autem non tam id reprehendunt, si remissius agatur, sed tantum studium tamque multam operam ponendam in eo non arbitrantur. erunt etiam, et ii quidem eruditi Graecis litteris, contemnentes Latinas, qui se dicant in Graecis legendis operam malle consumere. postremo aliquos futuros suspicor, qui me ad alias litteras vocent, genus hoc scribendi, etsi sit elegans, personae tamen et dignitatis esse negent.`
+	COMMIT_MESSAGE_BASE       = `Non eram nescius, Brute, cum, quae summis ingeniis exquisitaque doctrina philosophi Graeco sermone tractavissent, ea Latinis litteris mandaremus, fore ut hic noster labor in varias reprehensiones incurreret.`
 	BASE_SEPARATOR            = " "
 	MIN_COMMIT_MESSAGE_LENGTH = 2
 	MAX_COMMIT_MESSAGE_LENGTH = 8
