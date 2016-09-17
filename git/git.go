c1ac766ddfc32d41e8ee872b4a56f7d6707e9a20
KodaKDominus <kodakatsuro@gmail.com>
2016-09-17 16:41:39 +0200

    Simplify git command handling by using cmd.Dir
    instead of switching the directory in the call itself

--- git/git.go
+++ git/git.go
@@ -6,10 +6,6 @@ import (
 	"os/exec"
 )
 
-// PERM is the permission to create a directory/file for which the user
-// the user is allowed to read from/write to it.
-const PERM = 0755
-
 // Git is a simple interface the git command line tool.
 type Git interface {
 	Init()
@@ -25,25 +21,10 @@ type Repo struct {
 // Init initializes a git repo in its location.
 // If the location does not already exists, it will be created.
 func (git Repo) Init() {
-	if err := os.MkdirAll(git.Location, PERM); err != nil {
+	if err := os.MkdirAll(git.Location, 0755); err != nil {
 		log.Fatal(err)
 	}
-	err := exec.Command("git", "init", git.Location).Run()
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func getCwd() string {
-	currentWorkingDirectory, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
-	return currentWorkingDirectory
-}
-
-func changeDir(newDir string) {
-	if err := os.Chdir(newDir); err != nil {
+	if err := exec.Command("git", "init", git.Location).Run(); err != nil {
 		log.Fatal(err)
 	}
 }
@@ -51,21 +32,19 @@ func changeDir(newDir string) {
 // Add wraps the git add call and will change into the location of the git repo
 // add the file given by name and change back to the previous directory.
 func (git Repo) Add(filename string) {
-	currentDir := getCwd()
-	changeDir(git.Location)
-	if err := exec.Command("git", "add", filename).Run(); err != nil {
+	cmd := exec.Command("git", "add", filename)
+	cmd.Dir = git.Location
+	if err := cmd.Run(); err != nil {
 		log.Fatal(err)
 	}
-	changeDir(currentDir)
 }
 
 // Commit will change into the location of the git repo and execute git commit
 // with a message and date and change back to the previous directory.
 func (git Repo) Commit(message, date string) {
-	currentDir := getCwd()
-	changeDir(git.Location)
-	if err := exec.Command("git", "commit", "-m", message, "--date", date).Run(); err != nil {
+	cmd := exec.Command("git", "commit", "-m", message, "--date", date)
+	cmd.Dir = git.Location
+	if err := cmd.Run(); err != nil {
 		log.Fatal(err)
 	}
-	changeDir(currentDir)
 }
